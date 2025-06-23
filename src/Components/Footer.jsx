cc9d1e6bce1febda76f275ef2ccec7621a5a3147
dnguyen0091 <davidnguyen0091@icloud.com>
2025-06-22 22:17:01 -0400

    feat: initial logic for theme changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 // Footer component
 export default function Footer() {
   return (
-    <div className="bg-darkGrey2 flex justify-center items-center h-20 w-full fixed bottom-0 left-0 right-0">
+    <div className="bg-[--primary] flex justify-center items-center h-20 w-full fixed bottom-0 left-0 right-0">
       {/* container for the credits perhaps change it to a link to GitHub profile */}
       <div className="flex items-center gap-4">
-        <img src="src/assets/github.png" alt="github" id="githubLogo" />
-        <p className="text-white">Created by dnguyen0091©</p>
+        <img src="src/assets/github.png" alt="github" className="w-[4vh]"/>
+        <a href="https://github.com/dnguyen0091" target="_blank" className="text-[--text-primary]">Created by dnguyen0091 ©</a>
       </div>
     </div>
   );
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 // Header component
-export default function Header() {  return (
-    <div className="bg-darkGrey2 flex justify-start items-start h-auto w-full fixed top-0 left-0 right-0 px-4 py-2">
+export default function Header() {
+  return (
+    <div className="bg-[--primary] flex justify-start items-start h-auto w-full fixed top-0 left-0 right-0 px-4 py-2">
       <img src="src/assets/Logo.png" alt="Logo" className="w-[8vh]"/>
     </div>
   );
