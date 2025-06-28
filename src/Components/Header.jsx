a5c828fb5d4c2caa83c446bcc1edb6a5a23dc46d
dnguyen0091 <davidnguyen0091@icloud.com>
2025-06-28 11:20:11 -0400

    feat: refactored header into a sidebar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,13 @@
 // Header component
 export default function Header() {
   return (
-    <div className="bg-[--primary] flex justify-start items-start h-auto w-full fixed top-0 left-0 right-0 px-4 py-2">
-      <img src="src/assets/Logo.png" alt="Logo" className="w-[8vh]"/>
+    <div className="bg-[--primary] flex justify-start items-start h-[100vh] w-[10vw] fixed top-0 left-0 right-0 px-4 py-2">
+      {/* Header of SideBar */}
+      <div>
+        <img src="src/assets/Logo.png" alt="Logo" className="w-[3vw]"/>
+        <img src="src/assets/Icons/sidebarCollapse.png" alt="sideBarCollapse" className="w-[3vw]"/>
+      </div>
+      
     </div>
   );
 
